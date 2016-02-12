f9bf42cd8a866179c7726d17733735878a2c82ea
smuniapp <smuniapp@cisco.com>
2016-02-13 00:58:30 +0530

    Db final commit

--- UNIapp/src/com/example/app1/DBhelper.java
+++ UNIapp/src/com/example/app1/DBhelper.java
@@ -1,23 +1,29 @@
 package com.example.app1;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBhelper extends SQLiteOpenHelper{
     final static String DBNAME="sathish.db";
-	
+    final static String TABLENAME="sat";
+    final static String COL1="ID";
+    final static String COL2="NAME";
+    final static String COL3="AGE";
 	@SuppressLint("NewApi")
 	public DBhelper(Context context) {
 		super(context, DBNAME, null,1);
-SQLiteDatabase db=this.getWritableDatabase();		
+	
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
-		db.execSQL("create table sat (ID INTEGER, NAME TEXT)");
+		db.execSQL("create table "+TABLENAME +"("+COL1+" INTEGER, "+COL2+" TEXT, "+COL3+" INTEGER)");
 		
 	}
 
@@ -26,6 +32,49 @@ SQLiteDatabase db=this.getWritableDatabase();
 		db.execSQL("DROP TABLE IF EXISTS sat");
 		// TODO Auto-generated method stub
 		onCreate(db);
+	}
+	public boolean insertData(int id, String name,int age){
+		SQLiteDatabase db=this.getWritableDatabase();	
+		ContentValues cv = new ContentValues();
+		cv.put("id", id);
+		cv.put("name", name);
+		cv.put("age", age);
+		long result=db.insert(TABLENAME, null,cv);
+		if (result==-1)
+			return false;
+		else
+			return true;
+	}
+	
+	public Cursor getallData()
+	{
+		SQLiteDatabase db=this.getWritableDatabase();
+		Cursor res = db.rawQuery("select * from "+TABLENAME, null);
+		return res;
+	}
+	
+	public boolean updateData(int id, String name,int age){
+		SQLiteDatabase db=this.getWritableDatabase();	
+		ContentValues cv = new ContentValues();
+		cv.put("id", id);
+		cv.put("name", name);
+		cv.put("age", age);
+		long result=db.update(TABLENAME,cv,"ID = ?", new String[] {String.valueOf(id)});
+		if (result==-1)
+			return false;
+		else
+			return true;
+	}
+	
+	public boolean deleteData(int id){
+		SQLiteDatabase db=this.getWritableDatabase();	
+		ContentValues cv = new ContentValues();
+		cv.put("id", id);		
+		long result=db.delete(TABLENAME,"ID = ?", new String[] {String.valueOf(id)});
+		if (result==-1)
+			return false;
+		else
+			return true;
 	}
 
 }
--- UNIapp/src/com/example/app1/MainActivity.java
+++ UNIapp/src/com/example/app1/MainActivity.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -15,43 +17,111 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app1.R;
 
 public class MainActivity extends ActionBarActivity {
 
-    TextView tt;
-	EditText ed1,ed2 ;
-	Button btsave,btsavetoexcel;
-
+	TextView tt;
+	EditText ed0, ed1, ed2;
+	Button btsave, btview,btupdate,btdelete;
+	DBhelper db;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_main);
-		DBhelper db = new DBhelper(this);
+		db = new DBhelper(this);
 		tt = (TextView) findViewById(R.id.textsat);
+		ed0 = (EditText) findViewById(R.id.ids);
 		ed1 = (EditText) findViewById(R.id.name);
 		ed2 = (EditText) findViewById(R.id.age);
-		 btsave = (Button) findViewById(R.id.save);
-		 btsavetoexcel = (Button) findViewById(R.id.savetoexcel);		 
+		btsave = (Button) findViewById(R.id.save);
+		btview = (Button) findViewById(R.id.view);
+		btupdate = (Button) findViewById(R.id.btUpdate);
+		btdelete = (Button) findViewById(R.id.btDelete);
+		
 		tt.setText(R.string.sat1);
+
 		clickSave();
-		clickSaveToExcel();
+		showAll();
+		updateValue();
+		deleteValue();
 
 	}
-
-	private void clickSaveToExcel() {
-		btsavetoexcel.setOnClickListener(new OnClickListener() {
+    
+	private void deleteValue() {
+		btdelete.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				tt.setText(readFile("sathish"));
+				int s0 = Integer.parseInt(ed0.getText().toString());
+					boolean inserted = db.deleteData(s0);
+				if (inserted)
+					Toast.makeText(MainActivity.this, "Data deleted",
+							Toast.LENGTH_LONG).show();
+				else
+					Toast.makeText(MainActivity.this, "Data not delete",
+							Toast.LENGTH_LONG).show();
 				
 			}
 		});
 		
+	}
+
+	private void updateValue() {
+		btupdate.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int s0 = Integer.parseInt(ed0.getText().toString());
+				String s1 = ed1.getText().toString();
+				String s2 = ed2.getText().toString();
+				boolean inserted =db.updateData(s0, s1, Integer.parseInt(s2));
+				 
+				if (inserted)
+					Toast.makeText(MainActivity.this, "Data Update",
+							Toast.LENGTH_LONG).show();
+				else
+					Toast.makeText(MainActivity.this, "Data not updated",
+							Toast.LENGTH_LONG).show();
+				
+			}
+		});
+		
+	}
+
+	private void showAll() {
+		// TODO Auto-generated method stub
+		btview.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				Cursor res = db.getallData();
+				StringBuffer sb = new StringBuffer();
+				while (res.moveToNext()) {
+					sb.append("ID" + res.getInt(0) + "\n");
+					sb.append("NAME" + res.getString(1) + "\n");
+					sb.append("AGE" + res.getInt(2) + "\n\n");
+
+				}
+				showMessage("student", sb.toString());
+
+			}
+		});
+
+	}
+
+	public void showMessage(String Title, String msg) {
+
+		AlertDialog.Builder ab = new AlertDialog.Builder(this);
+		ab.setTitle(Title);
+		ab.setMessage(msg);
+		ab.show();
+
 	}
 
 	public void clickSave() {
@@ -59,18 +129,18 @@ public class MainActivity extends ActionBarActivity {
 
 			@Override
 			public void onClick(View v) {
-//				ed2.setFocusable(false);
-//				ed1.setFocusable(false);
-//
-//				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-//				imm.hideSoftInputFromWindow(ed2.getWindowToken(), 0);
-//
-//				btsave.setFocusable(true);
-				String s1 = "Name: " + ed1.getText().toString();
-				String s2 = "Age: " + ed2.getText().toString();
+				int s0 = Integer.parseInt(ed0.getText().toString());
+				String s1 = ed1.getText().toString();
+				String s2 = ed2.getText().toString();
 
-				saveFile("sathish", s1 + "," + s2 + ";");
-				
+				// saveFile("sathish", s1 + "," + s2 + ";");
+				boolean inserted = db.insertData(s0, s1, Integer.parseInt(s2));
+				if (inserted)
+					Toast.makeText(MainActivity.this, "Data inserted",
+							Toast.LENGTH_LONG).show();
+				else
+					Toast.makeText(MainActivity.this, "Data not inserted",
+							Toast.LENGTH_LONG).show();
 
 			}
 		});
@@ -78,7 +148,7 @@ public class MainActivity extends ActionBarActivity {
 
 	public void saveFile(String fileName, String ss) {
 		try {
-			
+
 			FileOutputStream fos = openFileOutput(fileName,
 					Context.MODE_PRIVATE);
 
